dcd43bb8ca893856263fe1849791177b9d46c3c6
Mihai Popa <mihai.popa@techquarter.io>
2022-08-18 17:49:44 +0300

    Configured redux store

---
+++ src/store/configureStore.ts
@@ -0,0 +1,26 @@
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+
+import createSagaMiddleware from 'redux-saga';
+import rootSaga from './rootSaga';
+import studentsReducer from '../features/student/reducers';
+
+// Combines all the reducers in to a root reducers
+const combinedReducer = combineReducers({
+  studentsSection: studentsReducer
+});
+
+const sagaMiddleware = createSagaMiddleware();
+/**
+ * Creates the redux store
+ * The store is the main place where the entire application states lives. It is basically an object with information and methods and actions to change that state
+ * reducer: takes a list of all reducers
+ * middleware: takes an array of additional functionalities that run on changes to the internal actions
+ */
+export const store = configureStore({
+  reducer: combinedReducer,
+  middleware: [sagaMiddleware],
+  // Checks if we can run the Redux plugin in dev tools
+  devTools: process.env.NODE_ENV !== 'production'
+});
+
+sagaMiddleware.run(rootSaga);
