312ce21ac858ec1ed337c5600ecc3680c055ba5c
Mihai Popa <mihai.popa@techquarter.io>
2022-08-18 17:52:20 +0300

    Display and create students

--- src/components/Students.tsx
+++ src/components/Students.tsx
@@ -1,9 +1,77 @@
 import '../index.css';
 
-import React, { useState } from 'react';
+import React, { MouseEvent, useEffect, useState } from 'react';
+import {
+  StateStructure,
+  studentsCountSelector,
+  studentsSelector
+} from '../features/student/selectors';
+import {
+  addStudent,
+  getStudents,
+  getStudentsFailed,
+  getStudentsSuccess
+} from '../features/student/actions';
+import { useDispatch, useSelector } from 'react-redux';
+
+import { Student } from '../types/Student';
+import { fetchStudents } from '../features/student/apiCalls';
+import { useNavigate } from 'react-router-dom';
+import { useUpdateEffect } from '../hooks/useUpdateEffect';
 
 export const Students = () => {
+  let navigate = useNavigate();
+  const dispatch = useDispatch();
+  const studentsCount: number = useSelector((state: StateStructure) =>
+    studentsCountSelector(state)
+  );
+
+  useEffect(() => {
+    //fetchStudents();
+    const example = async () => {
+      dispatch(getStudents());
+      try {
+        const response = await fetchStudents();
+        dispatch(getStudentsSuccess(response));
+      } catch (error) {
+        dispatch(getStudentsFailed());
+      }
+    };
+    example();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  useUpdateEffect(() => navigate('/students'), [studentsCount]);
   const [isInputActive, setIsInputActive] = useState(false);
+  const [nameText, setNameText] = useState('');
+  const [surNameText, setSurNameText] = useState('');
+  const [showErrorMsg, setShowErrorMsg] = useState(false);
+  const [showSuccessMsg, setShowSuccessMsg] = useState(false);
+
+  const onNameChange = (e: React.ChangeEvent<HTMLInputElement>, setterMethod: Function) => {
+    setShowErrorMsg(false);
+    setShowSuccessMsg(false);
+    setterMethod(e.target.value);
+  };
+
+  const handleSubmit = (e: MouseEvent) => {
+    addStudentReq(e);
+    setNameText('');
+    setSurNameText('');
+  }; //To be fixed
+
+  const students: Array<Student> = useSelector((state: StateStructure) => studentsSelector(state));
+
+  const addStudentReq = async (event: MouseEvent) => {
+    event.preventDefault();
+    const newStudent: Student = {
+      name: nameText,
+      surName: surNameText
+    };
+    dispatch(addStudent(newStudent));
+  };
+
+  const validForm: boolean = nameText.length !== 0 && surNameText.length !== 0;
 
   return (
     <div className="container">
@@ -13,14 +81,16 @@ export const Students = () => {
           <div className={isInputActive ? 'student-input active' : 'student-input'}>
             <form className="student-form" action="">
               <label htmlFor="">Name:</label>
-              <input type="text" />
+              <input type="text" onChange={e => onNameChange(e, setNameText)} />
               <label htmlFor="">Surname:</label>
-              <input type="text" />
-              <button className="submit-btn" type="submit" value="Submit">
+              <input type="text" onChange={e => onNameChange(e, setSurNameText)} />
+              <button disabled={!validForm} className="submit-btn" onClick={e => handleSubmit(e)}>
                 Submit
               </button>
             </form>
           </div>
+          {showSuccessMsg && <p>Student added successfully!</p>}
+          {showErrorMsg && <p>Error while adding student!</p>}
           <button
             className={isInputActive ? 'add-student-btn inactive' : 'add-student-btn'}
             onClick={() => {
@@ -39,7 +109,19 @@ export const Students = () => {
               <th>Grades</th>
             </tr>
           </thead>
-          <tbody></tbody>
+          <tbody>
+            {students.length > 0 &&
+              students.map((st: Student, index: number) => {
+                return (
+                  <tr key={`rok-${index}`}>
+                    <td>{st.name}</td>
+                    <td>{st.surName}</td>
+                    <td>{st.surName}</td>
+                    <td>{st.surName}</td>
+                  </tr>
+                );
+              })}
+          </tbody>
         </table>
       </div>
     </div>
